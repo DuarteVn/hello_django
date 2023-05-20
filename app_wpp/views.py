385ef037bfa1bc9c986dd6d5dd5ccdac4703c37a
VineDuarte <viniciusduarte18@gmail.com>
2023-05-20 15:00:59 -0300

    add /home/

--- app_wpp/views.py
+++ app_wpp/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from app_wpp.models import evento
 
 
-def hello(request, nome, idade):
-    
-    return HttpResponse('<h1> A soma de {} e {}</h1>'.format(nome, idade))
+def index(request):
+    return redirect("/home/")
+
+
+def lista_eventos(request):
+    Evento = evento.objects.all()
+
+    # usuario = request.user
+    # Evento = evento.objects.filter(usuario=usuario)
+    dados = {'eventos':Evento}
+    return render(request, 'home.html', dados)
+
--- aula_dio/urls.py
+++ aula_dio/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from app_wpp import views
+from django.views.generic import RedirectView #Pra que?
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>/', views.hello, name='Hello World')
+    path('home/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/home/'))
 ]
